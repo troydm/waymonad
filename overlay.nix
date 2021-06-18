a99967cf65507e05ef6053312bccc3e2f13b998a
Las Safin <las@protonmail.ch>
2021-06-18 14:22:31 +0000

    Avoid affecting the glibc derivation and revert libinput package change
    Otherwise we'd have to build a new glibc for waymonad for some reason
    I still don't quite understand.

--- overlay.nix
+++ overlay.nix
@@ -1,43 +1,27 @@
 _: pkgs: {
-  haskellPackages = pkgs.haskellPackages.override (old: {
-    overrides =
-      pkgs.lib.composeExtensions (old.overrides or (_: _: {}))
-      (self: super: {
-        hayland =
-          pkgs.haskell.lib.dontCheck
-          (self.callCabal2nix "hayland" ./hsroots/haskell-wayland { });
-        HFuse = self.callCabal2nix "HFuse" ./hfuse { };
-        hsroots =
-          pkgs.haskell.lib.overrideCabal
-          (self.callCabal2nix "hsroots" ./hsroots { input = pkgs.libinput; })
-          (current: {
-            doHaddock = false;
-            librarySystemDepends =
-              current.librarySystemDepends ++ [ pkgs.xorg.libX11 ];
-          });
-        hslibinput =
-          self.callCabal2nix "libinput" ./hsroots/libinput
-          { inherit (pkgs) libinput; };
-        waymonad =
-          pkgs.haskell.lib.overrideCabal
-          (self.callCabal2nix "waymonad" ./. { inherit (pkgs) libinput libxkbcommon; })
-          (current: {
-            librarySystemDepends = current.librarySystemDepends ++
-              [
-                pkgs.wayland pkgs.pkgconfig pkgs.wlroots pkgs.pixman
-                pkgs.fuse pkgs.gegl_0_4
-              ];
-          });
-        waymonad-scanner =
-            self.callCabal2nix "waymonad-scanner" ./waymonad-scanner { };
-        xkbcommon = pkgs.haskell.lib.overrideCabal (
-          self.callCabal2nix "xkbcommon" ./hsroots/haskell-xkbcommon
-          { inherit (pkgs) libxkbcommon; }
-        ) (current: {
-          librarySystemDepends =
-            current.librarySystemDepends ++ [ pkgs.linuxHeaders pkgs.glibc ];
-          doCheck = false;
+  haskellPackages = pkgs.haskellPackages.override {
+    overrides = self: super: {
+      hayland = self.callCabal2nix "hayland" ./hsroots/haskell-wayland {};
+      HFuse = self.callCabal2nix "HFuse" ./hfuse {};
+      hsroots =
+        pkgs.haskell.lib.overrideCabal
+        (self.callCabal2nix "hsroots" ./hsroots { input = pkgs.libinput; })
+        (o: { librarySystemDepends = o.librarySystemDepends ++ [ pkgs.xorg.libX11 ]; });
+      libinput =
+        self.callCabal2nix "libinput" ./hsroots/libinput { inherit (pkgs) libinput; };
+      waymonad =
+        pkgs.haskell.lib.overrideCabal
+        (self.callCabal2nix "waymonad" ./. {})
+        (o: {
+          librarySystemDepends = o.librarySystemDepends ++
+            (with pkgs; [ wayland pkgconfig wlroots pixman fuse gegl_0_4 ]);
         });
-      });
-  });
+      waymonad-scanner =
+        self.callCabal2nix "waymonad-scanner" ./waymonad-scanner {};
+      xkbcommon =
+        pkgs.haskell.lib.overrideCabal
+        (self.callCabal2nix "xkbcommon" ./hsroots/haskell-xkbcommon {})
+        (o: { librarySystemDepends = o.librarySystemDepends ++ (with pkgs; [ linuxHeaders ]); });
+    };
+  };
 }
--- shell.nix
+++ shell.nix
@@ -1,3 +1,3 @@
 let
   pkgs = import <nixpkgs> { overlays = [ (import ./overlay.nix) ]; };
-in pkgs.haskellPackages.shellFor { packages = p: [ p.waymonad p.hsroots p.hayland p.waymonad-scanner p.xkbcommon p.HFuse ]; }
+in pkgs.haskellPackages.shellFor { packages = p: [ p.waymonad p.hsroots p.hayland p.waymonad-scanner p.xkbcommon p.HFuse p.libinput ]; }
