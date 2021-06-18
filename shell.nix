bb4df054ec457a198cfd666633b10fb3b1d35f30
Las Safin <las@protonmail.ch>
2021-06-18 15:32:36 +0000

    Improve shell.nix

--- shell.nix
+++ shell.nix
@@ -1,3 +1,7 @@
 let
   pkgs = import <nixpkgs> { overlays = [ (import ./overlay.nix) ]; };
-in pkgs.haskellPackages.shellFor { packages = p: [ p.waymonad p.hsroots p.hayland p.waymonad-scanner p.xkbcommon p.HFuse p.libinput ]; }
+in pkgs.haskellPackages.shellFor {
+  packages = p: [ p.waymonad p.hsroots p.hayland p.waymonad-scanner p.xkbcommon p.HFuse p.libinput ];
+  withHoogle = true;
+  buildInputs = [ pkgs.cabal-install pkgs.cabal2nix ];
+}
