1d15add1be08bca808f585c58a27c2d97c1d77ea
Las Safin <las@protonmail.ch>
2021-06-19 16:26:44 +0000

    Use L-as/xkbcommon which has fixes

--- overlay.nix
+++ overlay.nix
@@ -1,4 +1,10 @@
-_: pkgs: {
+_: pkgs:
+let
+  xkbcommon_src = builtins.fetchTarball {
+    url = "https://github.com/L-as/haskell-xkbcommon/archive/fbf66b30fde9a1dbda1cb591d13d55ff295d49bc.tar.gz";
+    sha256 = "0n93ma0cqnv89060qnay5l54id5qplf30z00bv18f6g7iq0sqcpm";
+  };
+in {
   haskellPackages = pkgs.haskellPackages.override {
     overrides = self: super: {
       hayland = self.callPackage ./hayland/hayland.nix {};
@@ -6,7 +12,7 @@ _: pkgs: {
       libinput = self.callPackage ./libinput.nix { libinput = pkgs.libinput; };
       waymonad-scanner = self.callPackage ./waymonad-scanner/waymonad-scanner.nix {};
       waymonad = self.callPackage ./waymonad.nix {};
-      xkbcommon = self.callPackage ./xkbcommon.nix {}; # FIXME: just override the one in haskellPackages
+      xkbcommon = self.callPackage (import "${xkbcommon_src}/xkbcommon.nix") {};
     };
   };
 }
