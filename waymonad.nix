a45b04b8be8d3c4c7406b5860721bba1c034c07d
Las Safin <las@protonmail.ch>
2021-06-20 12:17:47 +0000

    Remove unnecessary arguments from waymonad.nix

--- waymonad.nix
+++ waymonad.nix
@@ -1,10 +1,9 @@
 { mkDerivation, base, bytestring, clock, composition, config-schema
 , config-value, containers, data-default, deepseq, directory
 , formatting, ghc-prim, hayland, HFuse, hsroots, lib, hslibinput
-, libxkbcommon, mtl, network, process, safe, semigroupoids, stm
+, mtl, network, process, safe, semigroupoids, stm
 , template-haskell, text, time, transformers, unix, unliftio
 , unliftio-core, waymonad-scanner, xdg-basedir, xkbcommon
-, wayland, pkgconfig, wlroots, pixman, fuse, gegl_0_4
 }:
 mkDerivation {
   pname = "waymonad";
@@ -19,7 +18,6 @@ mkDerivation {
     semigroupoids stm template-haskell text time transformers unix
     unliftio unliftio-core waymonad-scanner xdg-basedir xkbcommon
   ];
-  #libraryPkgconfigDepends = [ hslibinput libxkbcommon wayland pkgconfig wlroots pixman fuse gegl_0_4 ];
   executableHaskellDepends = [
     base containers hayland hsroots hslibinput text xkbcommon
   ];
