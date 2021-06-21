3eb4db8e17e9adaaabee91925a102a372d3477ca
Julius Marozas <marozas.julius@gmail.com>
2021-06-21 13:44:38 +0300

    Add haskell-language-server to shell.nix

--- shell.nix
+++ shell.nix
@@ -4,5 +4,5 @@ let
 in pkgs.haskellPackages.shellFor {
   packages = p: [ p.waymonad p.hsroots p.hayland p.waymonad-scanner ];
   withHoogle = true;
-  buildInputs = [ pkgs.cabal-install pkgs.cabal2nix ];
+  buildInputs = [ pkgs.cabal-install pkgs.cabal2nix pkgs.haskell-language-server ];
 }
